2083a416fdd3e73b0057d15f55dc8c625741406e
diwasneupane <diwas.neupane55@gmail.com>
2024-06-14 10:04:30 +0545

    Setting up the project

---
+++ cms_frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-slate-600 h-200 w-100">
+        <p className=" text-red-700 text-9xl p-80">Hello world!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
