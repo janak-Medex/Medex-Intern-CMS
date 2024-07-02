ab1557a0e0df16638249f761e5ab439a1f342107
diwasneupane <diwas.neupane55@gmail.com>
2024-07-02 10:06:08 +0545

    create component part merge completed

--- cms_frontend/src/components/createComponents.tsx
+++ cms_frontend/src/components/createComponents.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
-import { FaPlus, FaTrash, FaUpload, FaExpand } from "react-icons/fa";
+import React, { useState, useEffect, useRef, useMemo } from "react";
+import { FaPlus, FaTrash, FaUpload, FaExpand, FaEdit } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../http/axiosInstance";
 import { toast } from "react-toastify";
+import { Button, Modal, Tooltip, Input } from "antd";
+import { InfoCircleOutlined, SearchOutlined } from "@ant-design/icons";
 
 export interface FormField {
   [key: string]: any;
@@ -42,6 +44,22 @@ const CreateComponent: React.FC<Props> = ({
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const baseImageUrl = import.meta.env.VITE_APP_BASE_IMAGE_URL || "";
+
+  // for add schema rule
+
+  const [fieldName, setFieldName] = useState("");
+  const [fieldType, setFieldType] = useState("");
+  const [isRequired, setIsRequired] = useState(false);
+  // const [minLength, setMinLength] = useState<number | undefined>(undefined);
+  // const [maxLength, setMaxLength] = useState<number | undefined>(undefined);
+  // const [minValue, setMinValue] = useState<number | undefined>(undefined);
+  // const [maxValue, setMaxValue] = useState<number | undefined>(undefined);
+  const [schemaRulesData, setSchemaRulesData] = useState<any[]>([]);
+  const [isSchemaModalOpen, setIsSchemaModalOpen] = useState<boolean>(false);
+  const [editMode, setEditMode] = useState<boolean>(false); // New state for edit mode
+  const [editingRule, setEditingRule] = useState<any>(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredSchemaRules, setFilteredSchemaRules] = useState([]);
 
   useEffect(() => {
     if (initialComponent) {
@@ -181,9 +199,142 @@ const CreateComponent: React.FC<Props> = ({
   //   setIsModalOpen(false);
   // };
 
-  const handleAddSchemaRule = (newRule: SchemaRule) => {
+  // const handleAddSchemaRule = (newRule: SchemaRule) => {
+  //   setIsModalOpen(false);
+  //   toast.success("New schema rule added successfully!");
+  // };
+  // Open to modal for adding schema rule
+  const handleInsertRule = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setFieldName("");
+    setFieldType("");
+    setIsRequired(false);
+    // setMinLength(undefined);
+    // setMaxLength(undefined);
+    // setMinValue(undefined);
+    // setMaxValue(undefined);
+    setEditMode(false);
+    setEditingRule(null);
     setIsModalOpen(false);
-    toast.success("New schema rule added successfully!");
+  };
+
+  const handleAddSchemaRule = async () => {
+    if (!fieldName.trim() || !fieldType) {
+      toast.error("Field Name and Field Type are required");
+      return;
+    }
+
+    const newRule = {
+      fieldName,
+      type: fieldType,
+      required: isRequired,
+    };
+
+    try {
+      if (editMode && editingRule) {
+        const response = await axiosInstance.put(
+          `schemas/${editingRule._id}`,
+          newRule,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          toast.success("Schema rule updated successfully!");
+        } else {
+          toast.error("Failed to update schema rule.");
+        }
+      } else {
+        // Add new rule
+        const response = await axiosInstance.post("schemas", newRule, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.status === 200 || response.status === 201) {
+          toast.success("New schema rule added successfully!");
+        } else {
+          toast.error("Failed to add the new schema rule.");
+        }
+      }
+
+      handleCloseModal();
+      viewAllSchema();
+    } catch (error) {
+      toast.error("An error occurred while processing the schema rule.");
+    }
+  };
+
+  const viewAllSchema = async () => {
+    setIsSchemaModalOpen(true);
+    try {
+      const response = await axiosInstance.get("schemas");
+      setSchemaRulesData(response.data);
+      toast.success("Schema Rules fetched successfully!");
+    } catch (error) {
+      console.error("Error fetching schemas:", error);
+    }
+  };
+
+  const handleViewSchemaModal = () => {
+    setIsSchemaModalOpen(false);
+  };
+
+  const handleCloseSchemaModal = () => {
+    setIsSchemaModalOpen(false);
+  };
+
+  const onSchemaRuleDelete = async (id: string) => {
+    try {
+      const response = await axiosInstance.delete(`schemas/${id}`);
+      if (response.status === 200) {
+        toast.success("Schema Rule deleted successfully!");
+      }
+    } catch (error) {
+      console.error("Error deleting schema rule:", error);
+      toast.error("Failed to delete schema rule.");
+    }
+    viewAllSchema();
+  };
+
+  const handleEditSchemaRule = (rule: any) => {
+    setEditMode(true);
+    setEditingRule(rule);
+    setFieldName(rule.fieldName);
+    setFieldType(rule.type);
+    setIsRequired(rule.required);
+    setIsModalOpen(true);
+  };
+
+  useEffect(() => {
+    const filtered =
+      schemaRulesData?.data?.filter((rule) =>
+        rule.fieldName.toLowerCase().includes(searchTerm.toLowerCase())
+      ) || [];
+    setFilteredSchemaRules(filtered);
+    setCurrentPage(1);
+  }, [schemaRulesData, searchTerm]);
+
+  // for pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const totalPages = Math.ceil(filteredSchemaRules.length / itemsPerPage);
+
+  const currentData = filteredSchemaRules.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
   };
 
   return (
@@ -328,21 +479,286 @@ const CreateComponent: React.FC<Props> = ({
             <p className="text-red-500 text-xs mt-1">{errors.formFields}</p>
           )}
         </div>
-        <div className="flex justify-center mb-8">
+        <div className="flex gap-4 justify-center mb-8">
           <button
             type="submit"
-            className="px-4 py-3 text-sm text-white bg-teal-600 rounded-lg hover:bg-teal-700 focus:outline-none mr-4"
+            className="px-4 py-2 text-sm text-white bg-teal-600 rounded-lg hover:bg-teal-700 focus:outline-none "
           >
             {initialComponent ? "Update Component" : "Create Component"}
           </button>
           <button
             type="button"
-            // onClick={handleInsertRule}
-            className="px-4 py-3 text-sm text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none transition-colors duration-300"
+            onClick={handleInsertRule}
+            className="px-4 py-2 text-sm text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none transition-colors duration-300"
           >
             Add New Schema Rule
           </button>
+          <button
+            type="button"
+            className="px-4 py-2 text-sm text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none transition-colors duration-300"
+            onClick={viewAllSchema}
+          >
+            View all Schema
+          </button>
         </div>
+
+        {/* Modal to view all schema */}
+        <Modal
+          title={
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginBottom: "15px",
+              }}
+            >
+              <span>View all Schema Rules</span>
+              <Input
+                prefix={<SearchOutlined />}
+                placeholder="Search..."
+                style={{ width: 170, marginLeft: "auto", marginRight: "30px" }}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
+          }
+          open={isSchemaModalOpen}
+          onOk={handleViewSchemaModal}
+          onCancel={handleCloseSchemaModal}
+          width={700}
+          centered
+        >
+          <div>
+            {filteredSchemaRules?.length > 0 ? (
+              <>
+                <table className="w-full mb-4 border-collapse table-fixed">
+                  <thead className="cursor-pointer">
+                    <tr>
+                      <th className="border border-gray-300 px-4 py-2 bg-gray-100 text-left w-3/4">
+                        FieldName
+                      </th>
+                      <th className="border border-gray-300 px-4 py-2 bg-gray-100 text-left w-1/2">
+                        Type
+                      </th>
+                      <th className="border border-gray-300 px-4 py-2 bg-gray-100 text-left w-1/2">
+                        Required
+                      </th>
+                      <th className="border border-gray-300 px-4 py-2 bg-gray-100 text-left w-1/2">
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {currentData.map((data, index) => (
+                      <tr key={data._id || index}>
+                        <td className="border border-gray-300 px-4 py-2">
+                          <span className="flex justify-between">
+                            <span className="mr-12">{data.fieldName}</span>
+                          </span>
+                        </td>
+                        <td className="border border-gray-300 px-4 py-2">
+                          {data.type}
+                        </td>
+                        <td className="border border-gray-300 px-4 py-2">
+                          {String(data.required)}
+                        </td>
+                        <td className="border border-gray-300 px-4 py-2">
+                          <span className="flex gap-6">
+                            <button
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                onSchemaRuleDelete(data._id);
+                              }}
+                            >
+                              <FaTrash className="text-red-600" />
+                            </button>
+                            <button
+                              className="focus:outline-none"
+                              onClick={() => handleEditSchemaRule(data)}
+                            >
+                              <FaEdit className="text-gray-600" size={20} />
+                            </button>
+                          </span>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                {/* Pagination Controls */}
+                <div className="flex justify-center mt-4">
+                  <button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    className="px-4 py-2 mx-1 border border-gray-300 bg-gray-100 rounded"
+                  >
+                    Previous
+                  </button>
+                  <span className="px-4 py-2">
+                    Page {currentPage} of {totalPages}
+                  </span>
+                  <button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}
+                    className="px-4 py-2 mx-1 border border-gray-300 bg-gray-100 rounded"
+                  >
+                    Next
+                  </button>
+                </div>
+              </>
+            ) : (
+              <p>No matching rules found</p>
+            )}
+          </div>
+        </Modal>
+
+        <Modal
+          title={editMode ? "Edit Schema Rule" : "Add New Schema Rule"}
+          open={isModalOpen}
+          onOk={handleAddSchemaRule}
+          onCancel={handleCloseModal}
+          centered
+        >
+          <form className="space-y-4">
+            <div>
+              <label
+                htmlFor="fieldName"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Field Name
+              </label>
+              <input
+                type="text"
+                id="fieldName"
+                value={fieldName}
+                onChange={(e) => setFieldName(e.target.value)}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="fieldType"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Field Type
+              </label>
+              <select
+                id="fieldType"
+                value={fieldType}
+                onChange={(e) => setFieldType(e.target.value)}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              >
+                <option value="">
+                  {editMode ? fieldType : "Select a type"}
+                </option>
+                <option value="String">String</option>
+                <option value="Number">Number</option>
+                <option value="Boolean">Boolean</option>
+                <option value="Date">Date</option>
+              </select>
+            </div>
+
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="isRequired"
+                checked={isRequired}
+                onChange={(e) => setIsRequired(e.target.checked)}
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+              />
+              <label
+                htmlFor="isRequired"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Required
+              </label>
+            </div>
+
+            {/* <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label
+                  htmlFor="minLength"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Min Length
+                </label>
+                <input
+                  type="number"
+                  id="minLength"
+                  value={minLength || ""}
+                  onChange={(e) =>
+                    setMinLength(
+                      e.target.value ? Number(e.target.value) : undefined
+                    )
+                  }
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="maxLength"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Max Length
+                </label>
+                <input
+                  type="number"
+                  id="maxLength"
+                  value={maxLength || ""}
+                  onChange={(e) =>
+                    setMaxLength(
+                      e.target.value ? Number(e.target.value) : undefined
+                    )
+                  }
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label
+                  htmlFor="minValue"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Min Value
+                </label>
+                <input
+                  type="number"
+                  id="minValue"
+                  value={minValue || ""}
+                  onChange={(e) =>
+                    setMinValue(
+                      e.target.value ? Number(e.target.value) : undefined
+                    )
+                  }
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="maxValue"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Max Value
+                </label>
+                <input
+                  type="number"
+                  id="maxValue"
+                  value={maxValue || ""}
+                  onChange={(e) =>
+                    setMaxValue(
+                      e.target.value ? Number(e.target.value) : undefined
+                    )
+                  }
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+            </div> */}
+          </form>
+        </Modal>
       </form>
 
       {isFullscreen && imagePreview && (
