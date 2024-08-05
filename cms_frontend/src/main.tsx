5c770731ea66403ed3124d0998c8a6f15eae1372
diwasneupane <diwas.neupane55@gmail.com>
2024-08-05 10:06:00 +0545

    with validation impllemented

--- cms_frontend/src/main.tsx
+++ cms_frontend/src/main.tsx
@@ -5,12 +5,12 @@ import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-confirm-alert/src/react-confirm-alert.css";
 import "react-toastify/dist/ReactToastify.css";
-// import DynamicFormRenderer from "./templateForm/FormRender";
+import DynamicFormRenderer from "./templateForm/FormRender";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    {/* <DynamicFormRenderer /> */}
+    {/* <App /> */}
+    <DynamicFormRenderer />
     <ToastContainer position="top-right" autoClose={3000} />{" "}
   </React.StrictMode>
 );
